14bc5e3dbf5cf644ef19a3c10af295d3fff3f1e3
Pedro Palma <156723788+pedpalma@users.noreply.github.com>
2025-08-18 19:58:22 -0300

    update: js

--- js/pedidos.js
+++ js/pedidos.js
@@ -1,1 +1,17 @@
-document.querySelector(".total").textContent = document.querySelector(".qtd").textContent * document.querySelector(".unitario").textContent
+// document.querySelector(".total").textContent = document.querySelector(".qtd").textContent * document.querySelector(".unitario").textContent
+
+// Captura de encomendas da tabela
+var clientes = document.querySelectorAll(".cliente");
+
+// Estrutura de repetição para passar por todas as encomendas e calcular o total
+for(var count=0; count<clientes.length; count++){
+
+    // Qtd de encomendas
+    var qtd = clientes[count].querySelector(".qtd").textContent;
+
+    // Valor de cada encomenda
+    var unitario = clientes[count].querySelector(".unitario").textContent;
+
+    // Exibe o total
+    clientes[count].querySelector(".total").textContent = qtd * unitario;
+}
