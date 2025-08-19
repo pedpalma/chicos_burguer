22cce254f075c61b086ac2c257d82a72ff800daa
Pedro Palma <156723788+pedpalma@users.noreply.github.com>
2025-08-18 21:58:01 -0300

    ft: validação de erros column unitario

--- js/pedidos.js
+++ js/pedidos.js
@@ -4,7 +4,7 @@
 var clientes = document.querySelectorAll(".cliente");
 
 // Estrutura de repetição para passar por todas as encomendas e calcular o total
-for(var count=0; count<clientes.length; count++){
+for (var count = 0; count < clientes.length; count++) {
 
     // Qtd de encomendas
     var qtd = clientes[count].querySelector(".qtd").textContent;
@@ -13,7 +13,7 @@ for(var count=0; count<clientes.length; count++){
     var unitario = clientes[count].querySelector(".unitario").textContent;
 
     // Verifica se a qtd é valida
-    if(qtd < 1 || isNaN(qtd)){
+    if (qtd < 1 || isNaN(qtd)) {
 
         //A qtd é menor que 1 ou NaN
         clientes[count].querySelector(".qtd").textContent = "Quantidade Inválida!";
@@ -26,22 +26,37 @@ for(var count=0; count<clientes.length; count++){
 
         // Linha toda vermelha e texto em branco
         clientes[count].style.backgroundColor = "red";
+
         clientes[count].style.color = "white";
 
+    } else {
 
+        // Verifica se o unitario é valido
+        if (unitario < 1 || isNaN(unitario)) {
 
-    }else{
+            // Unitario é menor que o mínimo ou NaN
+            clientes[count].querySelector(".unitario").textContent = "Valor incorreto";
 
-        // Exibe o total
-        clientes[count].querySelector(".total").textContent = calcula_total(qtd,unitario);
+            // Linha toda vermelha e texto em branco
+            clientes[count].style.backgroundColor = "red";
+
+            clientes[count].style.color = "white";
+
+        } else {
+
+            // Exibe o total
+            clientes[count].querySelector(".total").textContent = calcula_total(qtd, unitario);
+
+        }
 
     }
+
 
 }
 
 //Função de calculo do valor total
-function calcula_total(qtd,unit){
-    
+function calcula_total(qtd, unit) {
+
     var total = 0;
 
     total = qtd * unit;
