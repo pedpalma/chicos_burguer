dc0c577d38661d533d328293e11693bcde226680
Pedro Palma <pedro.francisco.palma@gmail.com>
2025-11-24 19:41:24 -0300

    -fix: API

--- js/ecomendaAPI.js
+++ js/ecomendaAPI.js
@@ -1,17 +1,22 @@
 var botaoAPI = document.querySelector("#api-encomenda");
 
-botaoAPI.addEventListener("click", function(){
-    //Consome a API 
+botaoAPI.addEventListener("click", function() {
     var api = new XMLHttpRequest();
     api.open("GET", "http://localhost:3000/encomendas_web");
-    api.addEventListener("load", function(){
-        //Formata os dados vindos do JSON
-        var resposta = JSON.parse(api.responseText);
 
-        //Passa por cada item do JSON e adiciona na tabela
-        resposta.forEach(function(encomendaAPI) {
-                            adicionaEncomendaTabela(encomendaAPI);
-                        });
-    })
+    api.addEventListener("load", function() {
+
+        if (api.status == 200) {
+            var resposta = JSON.parse(api.responseText);
+
+            resposta.forEach(function(encomendaAPI) {
+                adicionaEncomendaTabela(encomendaAPI);
+            });
+
+        } else {
+            console.error("Erro ao carregar API:", api.status, api.responseText);
+        }
+    });
+
     api.send();
-})
+});
